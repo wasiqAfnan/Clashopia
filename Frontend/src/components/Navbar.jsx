b1059f3ca80106ebdde73c14c3cd182d17ddd79b
wasiqafnan <wansari964@yahoo.com>
2025-04-21 15:54:25 +0530

    Added favicon and also tweak navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Row, Col, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logo from "../assets/download.svg"
 
 function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          Clash Royale Cards
-        </Navbar.Brand>
+        <Row className="w-100 align-items-center text-center ">
+          {/* Left - Brand */}
+          <Col xs={4} className="text-start fw-bold" style={{ fontSize: "1.1rem" }}>
+            <Navbar.Brand as={Link} to="/">
+              Clash-O-Pia
+            </Navbar.Brand>
+          </Col>
 
-        <Nav className="ms-auto">
-          <Navbar.Text className="text-light">
-            Made with ❤️ by Wasiq Afnan
-          </Navbar.Text>
-        </Nav>
+          {/* Center - Logo */}
+          <Col xs={4}>
+            <Image
+              src={logo}
+              alt="Logo"
+              height="70"
+              className="d-inline-block align-top"
+              rounded
+            />
+          </Col>
+
+          {/* Right - Signature */}
+          <Col xs={4} className="text-end">
+            <Navbar.Text className="text-light" style={{ fontSize: "1.2rem" }}>
+              Made with ❤️ by Wasiq Afnan
+            </Navbar.Text>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
