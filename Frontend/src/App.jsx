b2377a064a23a7e0a78e479820663b85709d37b7
wasiqafnan <wansari964@yahoo.com>
2025-04-21 17:50:38 +0530

    Fixed a bug in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/Navbar";
 import CardList from "./components/CardList";
-import CardDetails from "./pages/CardDetails";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<CardList />} />
-        <Route path="/card/:id" element={<CardDetails />} />
       </Routes>
     </Router>
   );
