af2376a3513d254427fd4863ce62d5a46419f443
wasiqafnan <wansari964@yahoo.com>
2025-07-07 22:19:33 +0530

    added cors policy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://clashopia.vercel.app',
+}));
 
 // Import routes
 const cardRoutes = require("./routes/cardRoutes"); 
